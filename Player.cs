97628b274600702b75abf49668c9e993d5efd979
Selkie Slatcher <selkieslatcher@gmail.com>
2025-03-18 13:22:56 -0400

    ...

---
+++ Player.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	public override void _Ready() 
+	{
+		
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		
+	}
+}
