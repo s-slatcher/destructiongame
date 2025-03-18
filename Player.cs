2e1102872c8f886d01fef874f68ffb876d755dad
Selkie Slatcher <selkieslatcher@gmail.com>
2025-03-18 13:44:04 -0400

    add move script

--- Player.cs
+++ Player.cs
@@ -3,12 +3,22 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
+	[Export]
+	public float MoveSpeed = 200f;
 	public override void _Ready() 
 	{
-		
+		GD.Print(MoveSpeed);
 	}
 	public override void _PhysicsProcess(double delta)
 	{
-		
+		Velocity = GetMoveVector() * MoveSpeed;
+		GD.Print(Velocity);
+		MoveAndSlide();
+	}
+
+	private static Vector2 GetMoveVector()
+	{
+		Vector2 moveVector = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+		return moveVector.Normalized();
 	}
 }
